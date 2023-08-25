b010f1585c4be5757563cc3c9d1247fe52c4dd09
whoisrobb <107462273+whoisrobb@users.noreply.github.com>
2023-08-25 09:46:32 +0300

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,6 @@ import { login, register } from "../controllers/auth.js"
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello, wusgood')
-})
 router.post('/register', register)
 router.post('/login', login)
 
